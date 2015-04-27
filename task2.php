da98b438a031d058a0174f76e887251a16250022
Fabio Pinto <fabio@parallel.co.za>
2015-04-27 15:14:25 +0200

    Commited working task2.php file

--- task2.php
+++ task2.php
@@ -8,4 +8,20 @@
 
 *Create the array and call the function. 
 **/
+//Function 
+function task_2(&$assoc_array) {
+	
+	//some bolded title
+	echo '<strong>Key => Value :</strong></br><hr>';	
+
+	//Print out of assoc array key/value pairs
+	foreach($assoc_array as $key => $value) {
+		echo $key.' => '.$value.'</br>'; 
+	}
+}
+
+//Array decalration and population
+$assoc_array = array('sky' => 'blue', 'grass' => 'green', 'clouds' => 'white');
+//Call to function
+task_2($assoc_array)
 ?>
